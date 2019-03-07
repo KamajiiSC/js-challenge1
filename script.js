a97ccf9190a8ddaad7ca2af69f4d91a56325c250
KamajiiSC <brandonlschaen@gmail.com>
2019-03-07 15:00:36 -0500

    Created bool higherBmi and assigned whether mark has a higher bmi

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ const JOHN_HEIGHT = 1.7;
 let johnBmi = JOHN_MASS / (JOHN_HEIGHT * JOHN_HEIGHT);
 let markBmi= MARK_MASS / (MARK_HEIGHT * MARK_HEIGHT);
  
+let higherBmi = markBmi > johnBmi;
+
 console.log(johnBmi);
 console.log(markBmi);
