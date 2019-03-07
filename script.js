c1cf8faa4d12d295a3e0bae8372c42295c070354
KamajiiSC <brandonlschaen@gmail.com>
2019-03-07 15:05:18 -0500

    Added comments and made code more readable

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const MARK_MASS = 100;
-const MARK_HEIGHT = 1.8;
+const MARK_MASS = 100; //kg
+const MARK_HEIGHT = 1.8; //meters
+
 const JOHN_MASS = 85;
 const JOHN_HEIGHT = 1.7;
 
+//Calculate John and Marks Bmi's
 let johnBmi = JOHN_MASS / (JOHN_HEIGHT * JOHN_HEIGHT);
 let markBmi= MARK_MASS / (MARK_HEIGHT * MARK_HEIGHT);
  
+//Compare their bmi numbers
 let isBmiHigher = markBmi > johnBmi;
 
 //console.log(johnBmi);
