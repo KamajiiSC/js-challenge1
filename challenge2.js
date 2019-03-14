2ce3905a47bdcee471c67f817bbc1eda585711fd
KamajiiSC <brandonlschaen@gmail.com>
2019-03-14 15:25:37 -0400

    Created options for everyone to tie

--- challenge2.js
+++ challenge2.js
@@ -19,4 +19,12 @@ if (scoreJohn > scoreMary && scoreJohn > scoreMike){
   console.log("Mike wins out of the three with an average of " + scoreMike);
 }else if(scoreMary > scoreJohn && scoreMary > scoreMike){
   console.log("Mary wins out of the three with an average of " + scoreMary);
+}else if(scoreJohn === scoreMike && scoreJohn !== scoreMary ){
+  console.log("John and Mike tied with an average of " + scoreJohn);
+}else if(scoreJohn === scoreMary && scoreJohn !== scoreMike){
+  console.log("John and Mary tied with an average of " + scoreJohn);
+}else if(scoreMike === scoreMary && scoreMike !== scoreJohn){
+  console.log("Mike and Mary tied with an average of " + scoreMike);
+}else{
+  console.log("There was a three way tie with a score of " + scoreJohn);
 }
