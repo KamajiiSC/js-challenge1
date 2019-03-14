b38e2ef963cdac44211d2f6ae5b3c3f8e4e4fade
KamajiiSC <brandonlschaen@gmail.com>
2019-03-14 15:28:11 -0400

    Added comments to make it more understandable

--- challenge2.js
+++ challenge2.js
@@ -13,18 +13,25 @@ if(scoreJohn > scoreMike){
 }
 */
 
+// Compares to see if John wins
 if (scoreJohn > scoreMary && scoreJohn > scoreMike){
   console.log("John wins out of the three with an average of " + scoreJohn);
+// Compares to see if Mike wins
 }else if(scoreMike > scoreJohn && scoreMike > scoreMary){
   console.log("Mike wins out of the three with an average of " + scoreMike);
+// Compares to see if Mary wins
 }else if(scoreMary > scoreJohn && scoreMary > scoreMike){
   console.log("Mary wins out of the three with an average of " + scoreMary);
+// Handles a tie between John and mike
 }else if(scoreJohn === scoreMike && scoreJohn !== scoreMary ){
   console.log("John and Mike tied with an average of " + scoreJohn);
+// Handles a tie between John and Mary
 }else if(scoreJohn === scoreMary && scoreJohn !== scoreMike){
   console.log("John and Mary tied with an average of " + scoreJohn);
+// Handles a tie between Mike and Mary
 }else if(scoreMike === scoreMary && scoreMike !== scoreJohn){
   console.log("Mike and Mary tied with an average of " + scoreMike);
+// Handles a 3 way tie
 }else{
   console.log("There was a three way tie with a score of " + scoreJohn);
 }
