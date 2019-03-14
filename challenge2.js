5711a404b37e51bd8621bf5077b93f797997c6c1
KamajiiSC <brandonlschaen@gmail.com>
2019-03-14 15:08:56 -0400

    Created if else statement to compare John and Mikes scores and log the answer

--- challenge2.js
+++ challenge2.js
@@ -1,2 +1,9 @@
+// Adding then dividing their final scores to get an average score
 const scoreJohn = (89 + 120 + 103) / 3;
 const scoreMike = (116 + 94 + 123) / 3;
+
+if(scoreJohn > scoreMike){
+  console.log("John wins on average compared to Mike, John scores " + scoreJohn);
+} else{
+  console.log("Mike wins on average compared to John, Mike scores " + scoreMike);
+}
