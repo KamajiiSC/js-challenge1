afd86ec53d0b932a2471db07dbdea02e15c096d8
KamajiiSC <brandonlschaen@gmail.com>
2019-03-14 15:13:53 -0400

    Commented out old comparison to make new one

--- challenge2.js
+++ challenge2.js
@@ -3,6 +3,7 @@ const scoreJohn = (89 + 120 + 103) / 3;
 const scoreMike = (116 + 94 + 123) / 3;
 const scoreMary = (97 + 134 + 105) / 3;
 
+/*
 if(scoreJohn > scoreMike){
   console.log("John wins on average compared to Mike, John scores " + scoreJohn);
 } else if(scoreJohn === scoreMike){
@@ -10,3 +11,4 @@ if(scoreJohn > scoreMike){
 }else{
   console.log("Mike wins on average compared to John, Mike scores " + scoreMike);
 }
+*/
