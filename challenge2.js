c447adeeeaa0725846950e00eea755369de35aa5
KamajiiSC <brandonlschaen@gmail.com>
2019-03-14 15:12:04 -0400

    Declared scoreMary and her scores

--- challenge2.js
+++ challenge2.js
@@ -1,6 +1,7 @@
 // Adding then dividing their final scores to get an average score
 const scoreJohn = (89 + 120 + 103) / 3;
 const scoreMike = (116 + 94 + 123) / 3;
+const scoreMary = (97 + 134 + 105) / 3;
 
 if(scoreJohn > scoreMike){
   console.log("John wins on average compared to Mike, John scores " + scoreJohn);
